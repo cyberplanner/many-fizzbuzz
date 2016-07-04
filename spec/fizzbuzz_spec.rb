d1676e47756669f882e0ab03c096443deb7ea65d
cyberplanner <saleh.gfx@gmail.com>
2016-07-04 17:08:24 +0100

    Passed FizzBuzz #01

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,14 +3,14 @@ require 'fizzbuzz'
 describe "FizzBuzz" do
   
   it "Returns 'Fizz' when passed 3" do
-    expect (fizzbuzz(3)).to eq "Fizz"
+    expect(fizzbuzz(3)).to eq "Fizz"
   end
   
   it "Returns 'Buzz' when passed 5" do
-    expect (fizzbuzz(5)).to eq "Buzz"
+    expect(fizzbuzz(5)).to eq "Buzz"
   end
   
   it "Returns 'FizzBuzz when passed 15" do
-    expect (fizzbuzz(15)).to eq "FizzBuzz"
+    expect(fizzbuzz(15)).to eq "FizzBuzz"
   end
 end
