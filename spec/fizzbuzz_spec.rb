b1b3fbbd4c6161d5980055576f88bfbc3d62de8a
cyberplanner <saleh.gfx@gmail.com>
2016-07-04 16:55:25 +0100

    Added specs/testes

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require 'fizzbuzz'
+
+describe "FizzBuzz" do
+  
+  it "Returns 'Fizz' when passed 3" do
+    expect (fizzbuzz(3)).to eq "Fizz"
+  end
+  
+  it "Returns 'Buzz' when passed 5" do
+    expect (fizzbuzz(5)).to eq "Buzz"
+  end
+  
+  it "Returns 'FizzBuzz when passed 15" do
+    expect (fizzbuzz(15)).to eq "FizzBuzz"
+  end
+end
