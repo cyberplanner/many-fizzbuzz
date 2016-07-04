3dc584e30eeea1904a4b5951e02e0b7af998cd59
cyberplanner <saleh.gfx@gmail.com>
2016-07-04 18:26:05 +0100

    Another way to do fizzbuzz

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,11 @@
 def fizzbuzz num
-  num % 3 == 0 && num % 5 == 0 ? "FizzBuzz" : num % 5 == 0 ? "Buzz" : num % 3 == 0 ? "Fizz" : num
+  if num % 3 == 0 ||  num % 5 == 0
+  "#{"Fizz" if num % 3 == 0}#{"Buzz" if num % 5 == 0}" 
+  else
+    num
+  end
 end
+
 
 =begin
 p fizzbuzz(3)
@@ -8,3 +13,4 @@ p fizzbuzz(5)
 p fizzbuzz(15)
 p fizzbuzz(2)
 =end
+
