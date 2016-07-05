07e5ee7a19539eb5a042aa0a3b3427dda2885d48
cyberplanner <saleh.gfx@gmail.com>
2016-07-05 11:04:29 +0100

    updating README

---
+++ README.md
@@ -0,0 +1,9 @@
+# FizzBuzz
+
+FizzBuzz challenge: exploring different solutions for fizzbuzz.
+
+## How to use
+
+```shell
+ruby fizzbuzz.rb
+```
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,12 +1,22 @@
 def fizzbuzz num
-  num = p"FizzBuzz#{num}"[num%3<1?0:num%5<1?4:8,num%15<1?8:4]
+  if num % num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
+  elsif num % 3 == 0
+    "Fizz"
+  elsif num % 5 == 0
+    "Buzz"
+  else
+    num
+  end
 end
 
 
 
+=begin
 p fizzbuzz(3)
 p fizzbuzz(5)
 p fizzbuzz(15)
 p fizzbuzz(2)
+=end
 
 
